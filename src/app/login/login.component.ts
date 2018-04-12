2e1a85f233852719dac94c77daaab3a9506b9f3d
yuschang <yuschang@g.sce.pccu.edu.tw>
2018-04-12 17:11:51 +0800

    2.1 Login 小改造

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  template: `
+    <div>
+      <input #usernameRef type="text">
+      <input #passwordRef type="password">
+      <button (click)="onClick(usernameRef.value, passwordRef.value)" >Login</button>
+    </div>
+  `,
+  styles: []
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick(username, password){
+    console.log('username:' + username + '\n\r' + 'password:' + password);
+  }
+}
