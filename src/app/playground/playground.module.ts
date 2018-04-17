00512c4a81f8aa1bff37db14d2b39178cee8264f
yuschang <yuschang@g.sce.pccu.edu.tw>
2018-04-17 12:52:14 +0800

    5.5 子路由

--- src/app/playground/playground.module.ts
+++ src/app/playground/playground.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlaygroundComponent } from './playground.component';
 import { PlaygroundRoutingModule } from './playground-routing.module';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './one/three/three.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PlaygroundRoutingModule
   ],
-  declarations: [PlaygroundComponent]
+  declarations: [PlaygroundComponent, OneComponent, TwoComponent, ThreeComponent]
 })
 export class PlaygroundModule { }
