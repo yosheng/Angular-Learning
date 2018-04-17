06a0f1ac045edf9e2b324c60053cc1a17e2cb65d
yuschang <yuschang@g.sce.pccu.edu.tw>
2018-04-17 10:09:02 +0800

    5.2 驗證用戶 轉跳

--- src/app/todo/todo.routes.ts
+++ src/app/todo/todo.routes.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { TodoComponent } from './todo.component';
+import { AuthGuardService } from '../core/auth-guard.service';
 
 export const routes: Routes = [
   {
     path: 'todo/:filter',
+    canActivate: [AuthGuardService],
     component: TodoComponent
   }
 ];
