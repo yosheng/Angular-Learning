eb1252846c069e1b85967ef5e6279d1fe450d5c1
risenzhang <sad445566@gmail.com>
2018-04-22 21:00:25 +0800

    7.3.1 動畫 state 和 transition

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, state, trigger, style, transition, animate } from '@angular/core';
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
-  styleUrls: ['./playground.component.css']
+  styleUrls: ['./playground.component.css'],
+  animations: [
+    trigger('signal', [
+      state('void',  style({
+        'transform': 'translateY(-100%)'
+      })),
+      state('go',  style({
+        'background-color': 'green',
+        'height': '100px'
+      })),
+      state('stop',  style({
+        'background-color': 'red',
+        'height': '50px'
+      })),
+      transition('void => *', animate(5000))
+    ])
+  ]
 })
 export class PlaygroundComponent implements OnInit {
 
   birthday = new Date();
+  signal: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onGo() {
+    this.signal = 'go';
+  }
+
+  onStop() {
+    this.signal = 'stop';
+  }
 }
