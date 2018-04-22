2c40ee825e24fb1dd2282e1a52834c52a132f1cc
risenzhang <sad445566@gmail.com>
2018-04-22 09:53:55 +0800

    7.2 自帶動畫技能

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CoreModule } from './core/core.module';
 import { TodoModule } from './todo/todo.module';
@@ -20,6 +22,8 @@ import { MdlModule } from '@angular-mdl/core';
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
     MdlModule,
     CoreModule,
     TodoModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 // login.component.ts
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, trigger, state, style, transition, animate } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Auth, Photo } from '../domain/entities';
@@ -7,7 +7,19 @@ import { Auth, Photo } from '../domain/entities';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('loginState', [
+      state('inactive', style({
+        transform: 'scale(1)'
+      })),
+      state('active', style({
+        transform: 'scale(1.1)'
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+    ])
+  ]
 })
 export class LoginComponent implements OnInit {
 
@@ -16,6 +28,7 @@ export class LoginComponent implements OnInit {
   photo = '/assets/login_default_bg.jpg';
   slides: Photo[] = [];
   auth: Auth;
+  loginBtnState = 'inactive';
 
   constructor(
     @Inject('auth') private service,
@@ -53,4 +66,7 @@ export class LoginComponent implements OnInit {
     }, 20000);
   }
 
+  toggleLoginState(btnState: boolean){
+    this.loginBtnState = btnState ? 'active' : 'inactive';
+  }
 }
